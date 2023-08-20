6dac5f49e84b3db988394f25baa535c4d01b5f14
Kayk peep <kayksantana133@gmail.com>
2023-08-20 12:23:11 -0300

    const variable

---
+++ typerConversion/script.js
@@ -0,0 +1,21 @@
+/*
+let age = window.prompt('How are your Old?'); 
+console.log(age)
+
+age = Number(age);
+age += 1; 
+console.log("Hello Birthday! you are " + age +  " years old");
+*/
+
+// tipos de conversao 
+let x; 
+let y; 
+let z; 
+
+x = Number (3.14); 
+y = String ('pizza'); 
+z = Boolean (''); 
+
+console.log(x, typeof x); 
+console.log(y, typeof y ); 
+console.log(z, typeof z); 
