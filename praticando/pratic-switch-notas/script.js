5128113aeed854fd5327e1f91f22d49b08921746
Kayk peep <kayksantana133@gmail.com>
2023-08-22 18:58:27 -0300

    switch estudos

---
+++ praticando/pratic-switch-notas/script.js
@@ -0,0 +1,23 @@
+let notas = 'F'; 
+
+switch(notas) { 
+  case "A": 
+  console.log('Parabens, Nota Maxima!')
+  break
+  case "B": 
+  console.log('Parabens, Otima Nota!') 
+  break 
+  case "C":
+  console.log('Tem que Estudar Mais!')
+  break 
+  case "D": 
+  console.log("Recuperaçao!")
+  break
+  case "F": 
+  console.log("Reprovado!")
+  break
+  default: 
+  console.log("Não é uma nota válida!");
+
+
+}
