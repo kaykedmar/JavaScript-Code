3720bfb29156847c0831ec0b042023941ccfb68d
Kayk peep <kayksantana133@gmail.com>
2023-08-22 20:09:16 -0300

    operadores logicos

---
+++ pratic-operadores-logicos/script.js
@@ -0,0 +1,11 @@
+// Uso de Operadores Logicos || and && 
+
+let temp = 30; 
+
+if(temp > 0 && temp <= 30) {
+  console.log('O clima esta bom !')
+} else if (temp > 10 || temp < 15) { 
+  console.log('O Tempo esta Frio!')
+} else { 
+  console.log('O clima ta pessimo')
+}
